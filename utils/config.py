a98cdf9ac480ba9aea3498fdff950eb66c96b311
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-06 01:17:26 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -1,13 +1,15 @@
-learning_rate = 0.001
-decay_step = 20
+learning_rate = 0.01
+decay_step = 10
 decay_rate = 0.3
 epoch_num = 100
 voc_size = 2500
 batch_size = 20
 emb_size = 20
-lstm_hidden_size = 100
+lstm_hidden_size = 50
 f0_dim = 10
-linear_h1 = 100
+linear_shape = 100
+linear_mean = 50
+linear_std = 50
 
 
 #################
