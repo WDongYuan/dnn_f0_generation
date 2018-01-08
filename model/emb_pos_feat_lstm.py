2bab7ab7b4ffb6d8a75b165af5c1c3add5cc3ba7
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-08 15:34:37 -0500

    debug

--- model/emb_pos_feat_lstm.py
+++ model/emb_pos_feat_lstm.py
@@ -218,13 +218,14 @@ class EMB_POS_FEAT_LSTM(nn.Module):
 		##CONV
 		self.kernel_size = 5
 		self.padding_size = int((self.kernel_size-1)/2)
-		self.out_channel = 20
-		self.conv = nn.Sequential(
+		self.out_channel = 30
+		self.conv1 = nn.Sequential(
 			nn.Conv1d(1,self.out_channel,self.kernel_size*self.emb_concat_size,stride=self.emb_concat_size,padding=self.padding_size*self.emb_concat_size),
 			#nn.BatchNorm2d(self.out_channel),
-			nn.Tanh(),
-			nn.Conv1d(1,self.out_channel,self.kernel_size*self.emb_concat_size,stride=self.emb_concat_size,padding=self.padding_size*self.emb_concat_size),
 			nn.Tanh())
+		# self.conv2 = nn.Sequential(
+		# 	nn.Conv1d(self.out_channel,self.out_channel,self.kernel_size*self.emb_concat_size,stride=self.emb_concat_size,padding=self.padding_size*self.emb_concat_size),
+		# 	nn.Tanh())
 
 		##LSTM
 		self.lstm_layer = 1
