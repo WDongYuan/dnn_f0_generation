5ab3c4bb7937c4ebdbd6f46edd08bc98c7cb5e1c
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-08 15:48:20 -0500

    debug

--- model/emb_pos_feat_lstm.py
+++ model/emb_pos_feat_lstm.py
@@ -224,7 +224,7 @@ class EMB_POS_FEAT_LSTM(nn.Module):
 			#nn.BatchNorm2d(self.out_channel),
 			nn.Tanh())
 		self.conv2 = nn.Sequential(
-			nn.Conv1d(self.out_channel,self.out_channel,self.kernel_size*self.emb_concat_size,stride=self.emb_concat_size,padding=self.padding_size*self.emb_concat_size),
+			nn.Conv1d(1,self.out_channel,self.kernel_size*self.out_channel,stride=self.out_channel,padding=self.padding_size*self.out_channel),
 			nn.Tanh())
 
 		##LSTM
@@ -263,8 +263,8 @@ class EMB_POS_FEAT_LSTM(nn.Module):
 
 		emb = torch.cat((emb,pos,feat),dim=2)
 
-		conv_result = self.conv1(emb.view(self.batch_size,1,self.max_length*self.emb_concat_size))
-		conv_result = self.conv2(conv_result).permute(0,2,1)
+		conv_result = self.conv1(emb.view(self.batch_size,1,self.max_length*self.emb_concat_size)).permute(0,2,1)
+		conv_result = self.conv2(conv_result.view(self.batch_size,1,self.max_length*self.out_channel)).permute(0,2,1)
 
 		c_0 = self.init_hidden()
 		h_0 = self.init_hidden()
