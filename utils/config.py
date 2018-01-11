6d10d42e22f0dfabd8a6c75345768f7ee3b20e00
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-11 01:33:31 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -1,6 +1,6 @@
 cuda_flag = True
 learning_rate = 0.001
-decay_step = 1
+decay_step = 20
 decay_rate = 0.95
 epoch_num = 100
 voc_size = 2500
@@ -8,7 +8,7 @@ batch_size = 20
 emb_size = 10
 pos_emb_size = 10
 tone_emb_size = 10
-lstm_hidden_size = 200
+lstm_hidden_size = 100
 f0_dim = 10
 linear_h1 = 200
 
