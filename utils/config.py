e8ad200c225f1759da595aabfad1d16face13f5c
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-18 00:07:31 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -5,7 +5,7 @@ dct_flag = False
 
 learning_rate = 0.001
 decay_step = 1
-decay_rate = 0.95
+decay_rate = 0.97
 epoch_num = 100
 voc_size = 2500
 batch_size = 20
@@ -137,3 +137,18 @@ linear_h1 = 200
 # f0_dim = 10
 # linear_h1 = 200
 # 27.0
+
+##emb_feat_pos_lstm+cons_vowel_tone_lstm_phrase
+# learning_rate = 0.001
+# decay_step = 1
+# decay_rate = 0.95
+# epoch_num = 100
+# voc_size = 2500
+# batch_size = 20
+# emb_size = 10
+# pos_emb_size = 10
+# tone_emb_size = 10
+# lstm_hidden_size = 100
+# f0_dim = 10
+# linear_h1 = 200
+#26.7
