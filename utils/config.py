377da5eaf09dc7bcee7d7703080a39ebf03b5d1e
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-20 22:33:34 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -9,12 +9,12 @@ decay_rate = 0.95
 epoch_num = 100
 voc_size = 2500
 batch_size = 20
-emb_size = 50
+emb_size = 20
 pos_emb_size = 10
 tone_emb_size = 10
 lstm_hidden_size = 100
 f0_dim = 10
-linear_h1 = 100
+linear_h1 = 50
 
 
 #################
