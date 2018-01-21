729d38cf320f7333dbd9748dfa0f48fbc794e9d7
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-20 23:27:51 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -362,7 +362,7 @@ def Train(train_emb,train_pos,train_cons,train_vowel,train_pretone,train_tone,tr
 				min_loss = val_loss
 		if (epoch+1)%decay_step==0:
 			learning_rate *= decay_rate
-			print(optimizer.param_groups)
+			print(len(optimizer.param_groups))
 			for param_group in optimizer.param_groups:
 				param_group['lr'] = learning_rate
 			print("#####################################")
