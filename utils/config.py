43026435d03fda79d4d421aeeff300c5d0e4fc3e
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-20 22:05:58 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -1,4 +1,4 @@
-cuda_flag = False
+cuda_flag = True
 update_data = False
 dct_num = 5
 dct_flag = False
@@ -14,7 +14,7 @@ pos_emb_size = 10
 tone_emb_size = 10
 lstm_hidden_size = 100
 f0_dim = 10
-linear_h1 = 200
+linear_h1 = 100
 
 
 #################
