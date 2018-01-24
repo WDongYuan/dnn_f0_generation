bcb1f2e29ce452e51817555489674a224688858e
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-24 16:09:36 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -319,7 +319,7 @@ class PHRASE_MEAN_LSTM(nn.Module):
 		feat_h_0 = torch.cat((feat,emb,pos,pos_feat),dim=2)
 		feat_h_n, (_,_) = self.feat_lstm(feat_h_0,(h_0,c_0))
 		feat_h = self.feat_l1(feat_h_n)
-		feat_h = self.relu(feat_h)
+		feat_h = self.sigmoid(feat_h)
 		feat_h = self.feat_l2(feat_h)
 
 		c_0 = self.init_phrase_hidden()
@@ -331,7 +331,7 @@ class PHRASE_MEAN_LSTM(nn.Module):
 		ph_h = self.relu(ph_h)
 		ph_h = self.phrase_l2(ph_h)
 
-		h = feat_h+ph_h
+		h = ph_h.mul(feat_h)
 		# h = feat_h
 
 		################################################################################
--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ update_data = False
 dct_num = 5
 dct_flag = False
 
-learning_rate = 0.0005
+learning_rate = 0.001
 decay_step = 1
 decay_rate = 0.95
 epoch_num = 100
@@ -12,7 +12,7 @@ batch_size = 20
 emb_size = 10
 pos_emb_size = 10
 tone_emb_size = 10
-lstm_hidden_size = 50
+lstm_hidden_size = 100
 f0_dim = 10
 linear_h1 = 100
 
