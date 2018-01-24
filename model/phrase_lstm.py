c1cb99028efc2677cd9da59009bd6442b7a3f90a
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-24 01:14:08 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -73,9 +73,9 @@ class PHRASE_LSTM(nn.Module):
 		self.direction = 2 if self.bidirectional_flag else 1
 		# self.emb_lstm = nn.LSTM(self.emb_size+self.pos_emb_size, self.lstm_hidden_size,
 		# 	num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
-		self.feat_lstm = nn.LSTM(self.phrase_num+self.feat_size+self.emb_size+self.pos_emb_length*self.pos_emb_size+self.pos_feat_num, self.lstm_hidden_size,
+		self.feat_lstm = nn.LSTM(self.feat_size+self.emb_size+self.pos_emb_length*self.pos_emb_size+self.pos_feat_num, self.lstm_hidden_size,
 			num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
-		self.phrase_lstm = nn.LSTM(3*self.tone_emb_size, self.phrase_hidden_size,
+		self.phrase_lstm = nn.LSTM(self.phrase_num+3*self.tone_emb_size, self.phrase_hidden_size,
 			num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
 		# self.syl_lstm = nn.LSTM(3*self.tone_emb_size, self.lstm_hidden_size,
 		# 	num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
@@ -101,10 +101,10 @@ class PHRASE_LSTM(nn.Module):
 		self.phrase_l2 = nn.Linear(self.phrase_linear_size,self.f0_dim)
 		self.linear_init(self.phrase_l2)
 
-		# self.syl_l1 = nn.Linear(self.lstm_hidden_size*self.direction,self.linear_h1)
-		# self.linear_init(self.syl_l1)
-		# self.syl_l2 = nn.Linear(self.linear_h1,self.f0_dim)
-		# self.linear_init(self.syl_l2)
+		self.concat_l1 = nn.Linear(2*self.lstm_hidden_size*self.direction,self.linear_h1)
+		self.linear_init(self.concat_l1)
+		self.concat_l2 = nn.Linear(self.linear_h1,self.f0_dim)
+		self.linear_init(self.concat_l2)
 
 		# self.mean_l1 = nn.Linear(self.lstm_hidden_size*self.direction,self.linear_h1)
 
@@ -151,22 +151,27 @@ class PHRASE_LSTM(nn.Module):
 
 		# print(pos.size())
 		# print(pos_feat.size())
-		feat_h_0 = torch.cat((feat,emb,pos,pos_feat,phrase),dim=2)
+		feat_h_0 = torch.cat((feat,emb,pos,pos_feat),dim=2)
 		feat_h_n, (_,_) = self.feat_lstm(feat_h_0,(h_0,c_0))
-		feat_h = self.feat_l1(feat_h_n)
-		feat_h = self.tanh(feat_h)
-		feat_h = self.feat_l2(feat_h)
+		# feat_h = self.feat_l1(feat_h_n)
+		# feat_h = self.tanh(feat_h)
+		# feat_h = self.feat_l2(feat_h)
 
 		c_0 = self.init_phrase_hidden()
 		h_0 = self.init_phrase_hidden()
 
-		ph_h_0 = torch.cat((tone,cons,vowel),dim=2)
+		ph_h_0 = torch.cat((tone,cons,vowel,phrase),dim=2)
 		ph_h_n, (_,_) = self.phrase_lstm(ph_h_0,(h_0,c_0))
-		ph_h = self.phrase_l1(ph_h_n)
-		ph_h = self.relu(ph_h)
-		ph_h = self.phrase_l2(ph_h)
+		# ph_h = self.phrase_l1(ph_h_n)
+		# ph_h = self.relu(ph_h)
+		# ph_h = self.phrase_l2(ph_h)
 
-		h = feat_h+ph_h
+		concat_h = self.concat_l1(torch.cat((ph_h_n,feat_h_n),dim=2))
+		concat_h = self.relu(concat_h)
+		concat_h = self.concat_l2(concat_h)
+		h = concat_h
+
+		# h = feat_h+ph_h
 		# h = feat_h
 
 		h = h.view(self.batch_size,self.max_length*self.f0_dim)
