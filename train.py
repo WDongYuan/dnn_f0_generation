65e2ae2de3ad57883beee6bb55eeb1065d4eb740
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-24 21:28:07 -0500

    debug

--- train.py
+++ train.py
@@ -1072,7 +1072,8 @@ if __name__=="__main__":
 		#############################################################
 		learning_rate = config.learning_rate
 		optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-		print([param.keys() for param in model.parameters()])
+		tmp_param = [param.keys() for param in model.parameters()]
+		print(len(tmp_param))
 		decay_step = config.decay_step
 		decay_rate = config.decay_rate
 		epoch_num = config.epoch_num
