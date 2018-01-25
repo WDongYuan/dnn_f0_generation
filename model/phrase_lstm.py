6d4a73b26fa36d2bf173dd394a2c0af9cb19e4e2
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-24 21:07:00 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -489,7 +489,7 @@ def Train(train_emb,train_pos,train_pos_feat,train_cons,train_vowel,train_preton
 			learning_rate *= decay_rate
 			# print(str(optimizer.param_groups).decode("utf-8"))
 			for param_group in optimizer.param_groups:
-				print(param_group)
+				print(param_group.keys())
 				param_group['lr'] = learning_rate
 			print("#####################################")
 			print("learning rate: "+str(learning_rate))
