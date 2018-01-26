a5f35af375ddbe4250ca7cf5b12e0f2bb858a332
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-26 14:02:00 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -347,7 +347,7 @@ class PHRASE_MEAN_LSTM(nn.Module):
 		# concat_feat = self.conv1(concat_feat).permute(0,2,1)
 		# concat_feat = concat_feat.view(self.batch_size,self.max_length,self.concat_len)
 		emb_h_0 = torch.cat((emb,dep),dim=2)
-		emb_h_n, (emb_h_t,emb_c_t) = self.emb_lstm(dep,(h_0,c_0))
+		emb_h_n, (emb_h_t,emb_c_t) = self.emb_lstm(emb_h_0,(h_0,c_0))
 		emb_h = self.emb_l1(emb_h_n)
 		emb_h = self.tanh(emb_h)
 		emb_h = self.emb_l2(emb_h)
