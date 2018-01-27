8e868375d6b2a8339c8ce967e09c1f4648783578
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-27 15:23:28 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -175,7 +175,7 @@ class PHRASE_LSTM(nn.Module):
 		# feat_h_0 = feat
 		feat_h_n, (_,_) = self.feat_lstm(feat_h_0,(h_0,c_0))
 		feat_h = self.feat_l1(feat_h_n)
-		feat_h = self.tanh(feat_h)
+		feat_h = self.relu(feat_h)
 		feat_h = self.feat_l2(feat_h)
 
 		c_0 = self.init_phrase_hidden()
