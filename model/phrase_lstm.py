05b5e6df7675b6b4538f9f2dddecc17d50efba5d
WeidongYuan <weidongy@andrew.cmu.edu>
2018-01-27 21:42:28 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -266,7 +266,7 @@ class PHRASE_TEST_LSTM(nn.Module):
 		self.feat_lstm = nn.LSTM(self.emb_l_size+self.pos_emb_length*self.pos_emb_size+self.pos_feat_num+self.dep_num+self.feat_size,
 			self.lstm_hidden_size,num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
 
-		self.phrase_lstm = nn.LSTM(3*self.tone_emb_size+self.phrase_num,
+		self.phrase_lstm = nn.LSTM(self.grad_emb_size+3*self.tone_emb_size+self.phrase_num,
 			self.phrase_hidden_size,num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
 		# self.syl_lstm = nn.LSTM(3*self.tone_emb_size, self.lstm_hidden_size,
 		# 	num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
@@ -364,7 +364,7 @@ class PHRASE_TEST_LSTM(nn.Module):
 		c_0 = self.init_phrase_hidden()
 		h_0 = self.init_phrase_hidden()
 
-		ph_h_0 = torch.cat((cons,vowel,tone,phrase),dim=2)
+		ph_h_0 = torch.cat((grad_emb,cons,vowel,tone,phrase),dim=2)
 		# ph_h_0 = grad_emb
 		ph_h_n, (_,_) = self.phrase_lstm(ph_h_0,(h_0,c_0))
 		ph_h = self.phrase_l1(ph_h_n)
