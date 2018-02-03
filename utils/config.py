c6caf5353123ca86bb2e5f1a3f40139c02b8a176
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-02 23:24:13 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -3,14 +3,14 @@ update_data = False
 dct_num = 5
 dct_flag = False
 
-learning_rate = 0.01
+learning_rate = 0.005
 decay_step = 1
 decay_rate = 0.95
 epoch_num = 100
 voc_size = 2500
 batch_size = 20
 emb_size = 300
-pos_emb_size = 10
+pos_emb_size = 2
 tone_emb_size = 10
 lstm_hidden_size = 100
 f0_dim = 10
