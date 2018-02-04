4a791d67381bdda7138ea555f2fc9bc24bfde754
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-04 01:43:48 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -266,10 +266,10 @@ class PHRASE_TEST_LSTM(nn.Module):
 		self.feat_lstm = nn.LSTM(self.emb_l_size+self.pos_emb_length*self.pos_emb_size+self.pos_feat_num+self.feat_size,
 			self.lstm_hidden_size,num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
 
-		self.phrase_lstm = nn.LSTM(3*self.tone_emb_size+self.phrase_num+self.feat_size,
+		# self.phrase_lstm = nn.LSTM(3*self.tone_emb_size+self.phrase_num+self.feat_size,
+		# 	self.phrase_hidden_size,num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
+		self.phrase_lstm = nn.LSTM(self.tone_emb_size,
 			self.phrase_hidden_size,num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
-		# self.syl_lstm = nn.LSTM(3*self.tone_emb_size, self.lstm_hidden_size,
-		# 	num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
 
 
 		self.non_linear = nn.ReLU()
@@ -348,31 +348,31 @@ class PHRASE_TEST_LSTM(nn.Module):
 		vowel = self.vowel_embed(vowel)
 
 
-		c_0 = self.init_hidden()
-		h_0 = self.init_hidden()
+		# c_0 = self.init_hidden()
+		# h_0 = self.init_hidden()
 
-		# print(pos.size())
-		# print(pos_feat.size())
-		# dep = self.dep_lemb(dep)
-		emb = self.emb_l1(emb)
-		feat_h_0 = torch.cat((emb,feat,pos,pos_feat),dim=2)
-		feat_h_n, (_,_) = self.feat_lstm(feat_h_0,(h_0,c_0))
-		feat_h = self.feat_l1(feat_h_n)
-		feat_h = self.tanh(feat_h)
-		feat_h = self.feat_l2(feat_h)
+		# # print(pos.size())
+		# # print(pos_feat.size())
+		# # dep = self.dep_lemb(dep)
+		# emb = self.emb_l1(emb)
+		# feat_h_0 = torch.cat((emb,feat,pos,pos_feat),dim=2)
+		# feat_h_n, (_,_) = self.feat_lstm(feat_h_0,(h_0,c_0))
+		# feat_h = self.feat_l1(feat_h_n)
+		# feat_h = self.tanh(feat_h)
+		# feat_h = self.feat_l2(feat_h)
 
 		c_0 = self.init_phrase_hidden()
 		h_0 = self.init_phrase_hidden()
 
-		ph_h_0 = torch.cat((feat,cons,vowel,tone,phrase),dim=2)
-		# ph_h_0 = grad_emb
+		# ph_h_0 = torch.cat((feat,cons,vowel,tone,phrase),dim=2)
+		ph_h_0 = tone
 		ph_h_n, (_,_) = self.phrase_lstm(ph_h_0,(h_0,c_0))
 		ph_h = self.phrase_l1(ph_h_n)
 		ph_h = self.relu(ph_h)
 		ph_h = self.phrase_l2(ph_h)
 
-		h = feat_h+ph_h
-		# h = feat_h
+		# h = feat_h+ph_h
+		h = ph_h
 
 		h = h.view(self.batch_size,self.max_length*self.f0_dim)
 		################################################################################
