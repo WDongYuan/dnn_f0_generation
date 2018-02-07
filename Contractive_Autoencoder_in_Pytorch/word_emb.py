1646c9f5f761103fe0b35171d3baa7e503ce7ec2
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-06 21:55:06 -0500

    debug

--- Contractive_Autoencoder_in_Pytorch/word_emb.py
+++ Contractive_Autoencoder_in_Pytorch/word_emb.py
@@ -89,7 +89,7 @@ if __name__=="__main__":
 		# print(train_label[0:100])
 		test_data,test_label = get_data_label("../lstm_data/test",win_size)
 		# print(test_label[0:100])
-		vocab_size = 2500
+		vocab_size = 3601
 		# print(test_data.shape)
 		# print(test_label.shape)
 		
