66520817a64e6d569bbb5cdd3269a3fd511ba6b8
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-07 00:32:57 -0500

    debug

--- Contractive_Autoencoder_in_Pytorch/word_emb.py
+++ Contractive_Autoencoder_in_Pytorch/word_emb.py
@@ -22,10 +22,10 @@ def show_heatmap(arr):
 class CAE(nn.Module):
 	def __init__(self,win_size,vocab_size):
 		super(CAE, self).__init__()
-		self.h_size = 200
+		self.h_size = 100
 		self.vocab_size = vocab_size
 		self.win_size = win_size
-		self.emb_size = 200
+		self.emb_size = 100
 		self.batch_size = -1
 
 		self.embed = nn.Embedding(self.vocab_size, self.emb_size,padding_idx=0)
