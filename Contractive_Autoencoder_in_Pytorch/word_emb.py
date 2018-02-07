eb32ca379d587d42223e891ceafb751018dd84df
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-07 00:09:03 -0500

    debug

--- Contractive_Autoencoder_in_Pytorch/word_emb.py
+++ Contractive_Autoencoder_in_Pytorch/word_emb.py
@@ -130,7 +130,9 @@ if __name__=="__main__":
 		model.cuda()
 
 		# model.load_state_dict(torch.load("best_model_con"))
-		optimizer = optim.Adam([param for param in model.parameters() if param.requires_grad], lr = 0.001,weight_decay=0)
+		# optimizer = optim.Adam([param for param in model.parameters() if param.requires_grad], lr = 0.005,weight_decay=0)
+		print(len([param for param in model.parameters() if param.requires_grad]))
+		optimizer = optim.Adam([param for param in model.parameters() if param.requires_grad], lr = 0.005,weight_decay=0)
 		# optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 		val_recons = None
 		max_acc = 0
