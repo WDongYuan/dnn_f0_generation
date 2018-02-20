13cdb167ac3787d0f8c5b24ac9aafd10126253ee
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-20 00:15:58 -0500

    try to concat two lstm out feature

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -117,6 +117,9 @@ class PHRASE_LSTM(nn.Module):
 		self.phrase_l2 = nn.Linear(self.phrase_linear_size,self.f0_dim)
 		self.linear_init(self.phrase_l2)
 
+		self.comb_l1 = nn.Linear(2*self.f0_dim,2*self.f0_dim)
+		self.comb_l2 = nn.Linear(2*self.f0_dim,self.f0_dim)
+
 
 	def linear_init(self,layer,lower=-1,upper=1):
 		layer.weight.data.uniform_(lower, upper)
@@ -228,11 +231,17 @@ class PHRASE_LSTM(nn.Module):
 		ph_h = self.relu(ph_h)
 		ph_h = self.phrase_l2(ph_h)
 
-		h = feat_h+ph_h
+		h = torch.cat((feat_h,ph_h),dim=2)
+		h = self.comb_l1(h)
+		h = self.relu(h)
+		h = self.comb_l2(h)
+
+		# h = feat_h+ph_h
+		
 		# delta,delta_length = self.get_f0_delta(h)
-		delta,delta_length = self.get_self_f0_delta(h)
+		# delta,delta_length = self.get_self_f0_delta(h)
 		# delta,delta_length = self.get_mean_delta(h)
-		h = torch.cat((h,delta),dim=2)
+		# h = torch.cat((h,delta),dim=2)
 		
 
 		# h = h.view(self.batch_size,self.max_length*self.f0_dim)
@@ -455,10 +464,10 @@ def Train(train_emb,train_pos,train_pos_feat,train_cons,train_vowel,train_preton
 			outputs = model(train_emb_batch,train_pos_batch,train_pos_feat_batch,train_cons_batch,train_vowel_batch,
 				train_pretone_batch,train_tone_batch,train_postone_batch,train_feat_batch,train_phrase_batch,train_dep_batch,train_len_batch)
 			
-			delta,delta_length = model.get_self_f0_delta(train_f0_batch)
+			# delta,delta_length = model.get_self_f0_delta(train_f0_batch)
 			# # delta,delta_length = model.get_f0_delta(train_f0_batch)
 			# # delta,delta_length = model.get_mean_delta(train_f0_batch)
-			train_f0_batch = torch.cat((train_f0_batch,delta),dim=2)
+			# train_f0_batch = torch.cat((train_f0_batch,delta),dim=2)
 
 			loss = LF(outputs,train_f0_batch)
 			loss.backward()
