22a511073fad762e53843c07c1e08f01b7cf1a14
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-20 22:25:17 -0500

    debug

--- utils/config.py
+++ utils/config.py
@@ -3,9 +3,9 @@ update_data = False
 dct_num = 5
 dct_flag = False
 
-learning_rate = 0.005
+learning_rate = 0.001
 decay_step = 1
-decay_rate = 0.98
+decay_rate = 0.95
 epoch_num = 100
 voc_size = 2500
 batch_size = 20
