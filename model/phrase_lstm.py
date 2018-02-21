df8a24f135ac7eae30493b334ba7b6f48eec7ad7
WeidongYuan <weidongy@andrew.cmu.edu>
2018-02-21 01:31:28 -0500

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -412,14 +412,14 @@ class TEST_MODEL(nn.Module):
 		emb = self.emb_l1(emb)
 
 		all_feat = torch.cat((emb,feat,pos,pos_feat,tone,cons,vowel,phrase),dim=2)
-		y = self.mlp(all_feat)
+		# y = self.mlp(all_feat)
 
 		# c_0 = self.init_hidden()
 		# h_0 = self.init_hidden()
 		# h_n, (_,_) = self.feat_lstm(all_feat,(h_0,c_0))
 		# y = self.lstm_l(h_n)
 
-		# y = self.ngram_mlp(self.get_ngram(all_feat,self.ngram_side))
+		y = self.ngram_mlp(self.get_ngram(all_feat,self.ngram_side))
 		return y
 
 class NGram(nn.Module):
@@ -605,6 +605,7 @@ def Validate(model,val_emb,val_pos,val_pos_feat,val_cons,val_vowel,val_pretone,v
 	loss = loss_arr.mean()
 
 	if save_prediction!="":
+		print("loss: "+str(loss))
 		np.savetxt(save_prediction,prediction,delimiter=" ",fmt="%.3f")
 
 	return loss,result.reshape((val_f0_shape))
