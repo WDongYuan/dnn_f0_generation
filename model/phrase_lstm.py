fa1575efcfdbcff503dd0ff974a088d3fdd3966a
WeidongYuan <weidongy@andrew.cmu.edu>
2018-03-21 03:58:05 -0400

    debug

--- model/phrase_lstm.py
+++ model/phrase_lstm.py
@@ -307,7 +307,7 @@ class TEST_MODEL(nn.Module):
 		##LSTM
 		self.lstm_layer = 1
 		self.bidirectional_flag = True
-		self.direction = 2 if self.bidirectional_flag else 1
+		self.direction = 1 if self.bidirectional_flag else 1
 		# self.emb_lstm = nn.LSTM(self.emb_size+self.pos_emb_size, self.lstm_hidden_size,
 		# 	num_layers=self.lstm_layer,bidirectional=self.bidirectional_flag,batch_first=True)
 		self.feat_lstm = nn.LSTM(self.emb_l_size+self.feat_size+self.pos_emb_length*self.pos_emb_size+self.pos_feat_num,self.lstm_hidden_size,
